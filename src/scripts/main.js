45d8e4950aaef661fed589007cfe31ccbf4b8b32
Ricardo Rosendo <88691117+RicardoRosendo98@users.noreply.github.com>
2023-03-17 17:29:09 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+const dataDoEvento = new Date("jul 24, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}Dias ${horasAteOEvento}Horas ${minutosAteOEvento}Minutos ${segundosAteOEvento}Segundos`
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'evento expirado'
+    }
+
+}, 1000)
